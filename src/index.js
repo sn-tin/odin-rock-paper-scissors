c1cc9e825b9361fb563fc273adaf3c7d31549860
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-04-26 20:47:53 +0800

    Install parcel sass

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ * Computer Play
+ */
+
+// function computerPlay(playerSelection, computerSelection) {
+//     for (i = 0; i <= 3; i++) {
+//         if (i = 0)
+//     }
+// }
+
+// function playRound(playerSelection, computerSelection) {
+//     // your code here!
+//   }
+  
+//   const playerSelection = "rock";
+//   const computerSelection = computerPlay();
+//   console.log(playRound(playerSelection, computerSelection));
+
+// function game() {
+
+// }
