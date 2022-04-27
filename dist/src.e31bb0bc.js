89b715222d9a21b018fd871ac51ccf77a10f74fc
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-04-27 18:46:48 +0800

    Move images folder from src to main folder

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -123,78 +123,92 @@ document.querySelector(".reload-btn").addEventListener("click", function handleC
   var randomNumber = Math.floor(Math.random() * 13) + 1;
 
   for (var i = 0; i <= randomNumber; i++) {
-    var svtImage = document.querySelector(".svt-img").src;
-    var svtName = document.querySelector(".svt-name").textContent;
+    var svtImage = document.querySelector(".svt-img");
+    var svtName = document.querySelector(".svt-name");
 
     switch (true) {
       case i === 1:
-        svtImage = "./images/svt-pilot-1.jpg";
-        svtName = "Choi Seungcheol";
+        // svtImage.src = "./images/svt-pilot-1.jpg";
+        svtImage.setAttribute("src", "./images/svt-pilot-1.jpg");
+        svtName.textContent = "Choi Seungcheol";
         break;
 
       case i === 2:
-        svtImage = "./images/svt-pilot-2.jpg";
-        svtName = "Yoon Jeonghan";
+        // svtImage.src = "./images/svt-pilot-2.jpg";
+        svtImage.setAttribute("src", "./images/svt-pilot-2.jpg");
+        svtName.textContent = "Yoon Jeonghan";
         break;
 
       case i === 3:
-        svtImage = "./images/svt-pilot-3.jpg";
-        svtName = "Joshua Hong";
+        // svtImage.src = "./images/svt-pilot-3.jpg";
+        svtImage.setAttribute("src", "./images/svt-pilot-3.jpg");
+        svtName.textContent = "Joshua Hong";
         break;
 
       case i === 4:
-        svtImage = "./images/svt-pilot-4.jpg";
-        svtName = "Wen Junhui";
+        // svtImage.src = "./images/svt-pilot-4.jpg";
+        svtImage.setAttribute("src", "./images/svt-pilot-4.jpg");
+        svtName.textContent = "Wen Junhui";
         break;
 
       case i === 5:
-        svtImage = "./images/svt-pilot-5.jpg";
-        svtName = "Kwon Hoshi";
+        // svtImage.src = "./images/svt-pilot-5.jpg";
+        svtImage.setAttribute("src", "./images/svt-pilot-5.jpg");
+        svtName.textContent = "Kwon Hoshi";
         break;
 
       case i === 6:
-        svtImage = "./images/svt-pilot-6.jpg";
-        svtName = "Jeon Wonwoo";
+        // svtImage.src = "./images/svt-pilot-6.jpg";
+        svtImage.setAttribute("src", "./images/svt-pilot-6.jpg");
+        svtName.textContent = "Jeon Wonwoo";
         break;
 
       case i === 7:
-        svtImage = "./images/svt-pilot-7.jpg";
-        svtName = "Lee Jihoon";
+        // svtImage.src = "./images/svt-pilot-7.jpg";
+        svtImage.setAttribute("src", "./images/svt-pilot-7.jpg");
+        svtName.textContent = "Lee Jihoon";
         break;
 
       case i === 8:
-        svtImage = "./images/svt-pilot-8.jpg";
-        svtName = "Xu Minghao";
+        // svtImage.src = "./images/svt-pilot-8.jpg";
+        svtImage.setAttribute("src", "./images/svt-pilot-8.jpg");
+        svtName.textContent = "Xu Minghao";
         break;
 
       case i == 9:
-        svtImage = "./images/svt-pilot-9.jpg";
-        svtName = "Kim Mingyu";
+        // svtImage.src = "./images/svt-pilot-9.jpg";
+        svtImage.setAttribute("src", "./images/svt-pilot-9.jpg");
+        svtName.textContent = "Kim Mingyu";
         break;
 
       case i === 10:
-        svtImage = "./images/svt-pilot-10.jpg";
-        svtName = "Lee Dokyeom";
+        // svtImage.src = "./images/svt-pilot-10.jpg";
+        svtImage.setAttribute("src", "./images/svt-pilot-10.jpg");
+        svtName.textContent = "Lee Dokyeom";
         break;
 
       case i === 11:
-        svtImage = "./images/svt-pilot-11.jpg";
-        svtName = "Boo Seungkwan";
+        // svtImage.src = "./images/svt-pilot-11.jpg";
+        svtImage.setAttribute("src", "./images/svt-pilot-11.jpg");
+        svtName.textContent = "Boo Seungkwan";
         break;
 
       case i === 12:
-        svtImage = "./images/svt-pilot-12.jpg";
-        svtName = "Vernon Chwe";
+        // svtImage.src = "./images/svt-pilot-12.jpg";
+        svtImage.setAttribute("src", "./images/svt-pilot-12.jpg");
+        svtName.textContent = "Vernon Chwe";
         break;
 
       case i === 13:
-        svtImage = "./images/svt-pilot-13.jpg";
-        svtName = "Lee Dino";
+        // svtImage.src = "./images/svt-pilot-13.jpg";
+        svtImage.setAttribute("src", "./images/svt-pilot-13.jpg");
+        svtName.textContent = "Lee Dino";
         break;
 
       default:
-        svtImage = "./images/svt-pilot-13.jpg";
-        svtName = "Team SVT";
+        // svtImage.src = "./images/svt-pilot-13.jpg";
+        svtImage.setAttribute("src", "./images/svt-pilot-13.jpg");
+        svtName.textContent = "Team SVT";
         break;
     }
   }
@@ -227,7 +241,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39535" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44919" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
